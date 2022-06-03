297fe1867953096a86337ed98581f0b920318e47
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2022-06-03 18:12:06 +0200

    Pagina privacy e copyright

---
+++ source/copyright.blade.php
@@ -0,0 +1,13 @@
+---
+title: Copyright
+description: Informazioni sul copyright
+---
+@extends('_layouts.main')
+
+@section('body')
+    <h1>Copyright</h1>
+
+
+
+    
+@endsection
---
+++ source/privacy.blade.php
@@ -0,0 +1,13 @@
+---
+title: Privacy
+description: Privacy e trattamento dei dati
+---
+@extends('_layouts.main')
+
+@section('body')
+    <h1>Privacy</h1>
+
+
+
+    
+@endsection
