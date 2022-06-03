fdde82627c9c7efb0b5fccbd903c605ef5c743a2
Alberto Peripolli <info@albertoperipolli.com>
2022-06-03 15:31:36 +0200

    Mailchimp fix

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -4,7 +4,7 @@
         <form action="https://laravel-news.us17.list-manage.com/subscribe/post?u=e32abc9cc1622d81b1e9308c8&id=cb256d4002" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
                 <h2>Iscriviti alla nostra newsletter</h2>
-                <div class="mb-1 text-sm">Inviando questa form stai accettando la nostra <a href="https://www.iubenda.com/privacy-policy/89440925" target="_blank">Privacy Policy</a>.</div>
+                <div class="mb-2 text-xs">Cliccando Invia accetti la nostra <a href="https://www.iubenda.com/privacy-policy/89440925" target="_blank">Privacy Policy</a>.<br/>Una volta iscritto riceverai una email di conferma.</div>
                 <div class="mc-field-group">
                     <label for="mce-EMAIL">Indirizzo email <span class="asterisk">*</span></label>
                     <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Indirizzo email">
@@ -12,7 +12,7 @@
 
                 <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e32abc9cc1622d81b1e9308c8_cb256d4002" tabindex="-1" value=""></div>
                 <div class="clear"><input type="submit" value="Invia" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                <div id="mce-responses" class="clear foot pt-10">
+                <div id="mce-responses" class="clear foot pt-6">
                     <div class="response" id="mce-error-response" style="display:none"></div>
                     <div class="response" id="mce-success-response" style="display:none"></div>
                 </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
