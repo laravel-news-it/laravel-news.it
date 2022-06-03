9236853d5b8394291f9a9edb3404f11068f7fa68
Alberto Peripolli <info@albertoperipolli.com>
2022-06-03 16:20:25 +0200

    carca

--- source/_assets/js/components/Search.vue
+++ source/_assets/js/components/Search.vue
@@ -14,7 +14,7 @@
                 :class="{ 'transition-border': query }"
                 autocomplete="off"
                 name="search"
-                placeholder="Carca"
+                placeholder="Cerca"
                 type="text"
                 @keyup.esc="reset"
                 @blur="reset"
