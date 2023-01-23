6103f4dbb597189dbaf88f2e125f5d7e4668d3d9
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2023-01-23 18:14:20 +0100

    articolo: Semplificare le immagini Docker con Octane

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -13,6 +13,7 @@ hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown')
 hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
 hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
 hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
+hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'));
 
 document.querySelectorAll('pre code').forEach((block) => {
     hljs.highlightBlock(block);
